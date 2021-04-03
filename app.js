22ff77adea8a0de5ecd0334d7b22e20df3196f1d
Adarsh Pathak <adarsh24699@gmail.com>
2021-04-03 20:03:42 +0530

    Database Encryption

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -15,10 +16,13 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true, useUnifiedTopology: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
-};
+});
+
+const secret = "Thisisourlittlesecrets.";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -60,11 +64,8 @@ app.post("/login", function(req, res){
         res.render("secrets");
       }
     }
-  });  
+  });
 });
-
-
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
